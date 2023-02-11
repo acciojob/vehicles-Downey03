59394834277a282e063ce139764ce5b28b5c06af
Karthick Raj K <karthickraj2001@gmail.com>
2023-02-11 11:33:04 +0530

    Completed with a need of few changes in accelerate fn in f1 class

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,13 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,2,6,isManual,"Racing",2);
+
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = rate+getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
+
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,12 +22,19 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
-            //Stop the car, set gear as 1
+            setGears(1);
+            return;
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
+            if(getCurrentSpeed()>0 && getCurrentSpeed()<=50) setGears(1);
+            if(getCurrentSpeed()>50 && getCurrentSpeed()<=100) setGears(2);
+            if(getCurrentSpeed()>100 && getCurrentSpeed()<=150) setGears(3);
+            if(getCurrentSpeed()>150 && getCurrentSpeed()<=200) setGears(4);
+            if(getCurrentSpeed()>200 && getCurrentSpeed()<=250) setGears(5);
+            if( getCurrentSpeed()<250) setGears(6);
         }
     }
 }
